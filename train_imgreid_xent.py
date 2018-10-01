9e1d2a8e94e28a8b90076488f2aae0220a16c3dd
luke <zzanzza@finger-plus.com>
2018-10-02 02:50:25 +0900

    로그 삭제 수정

--- train_imgreid_xent.py
+++ train_imgreid_xent.py
@@ -112,16 +112,16 @@ def main():
         sys.stdout = Logger(osp.join(args.save_dir, 'log_train.txt'))
     else:
         sys.stdout = Logger(osp.join(args.save_dir, 'log_test.txt'))
-    #print("==========\nArgs:{}\n==========".format(args))
+    print("==========\nArgs:{}\n==========".format(args))
 
     if use_gpu:
-        #print("Currently using GPU {}".format(args.gpu_devices))
+        print("Currently using GPU {}".format(args.gpu_devices))
         cudnn.benchmark = True
         torch.cuda.manual_seed_all(args.seed)
     else:
-        #print("Currently using CPU (GPU is highly recommended)")
+        print("Currently using CPU (GPU is highly recommended)")
 
-    #print("Initializing dataset {}".format(args.dataset))
+    print("Initializing dataset {}".format(args.dataset))
     dataset = data_manager.init_imgreid_dataset(
         root=args.root, name=args.dataset, split_id=args.split_id,
         cuhk03_labeled=args.cuhk03_labeled, cuhk03_classic_split=args.cuhk03_classic_split,
@@ -165,9 +165,9 @@ def main():
         pin_memory=pin_memory, drop_last=False,
     )
 
-    #print("Initializing model: {}".format(args.arch))
+    print("Initializing model: {}".format(args.arch))
     model = models.init_model(name=args.arch, num_classes=dataset.num_train_pids, loss={'xent'}, use_gpu=use_gpu)
-    #print("Model size: {:.3f} M".format(count_num_param(model)))
+    print("Model size: {:.3f} M".format(count_num_param(model)))
     # summary(model, (3, 160, 64))
 
     criterion = CrossEntropyLabelSmooth(num_classes=dataset.num_train_pids, use_gpu=use_gpu)
@@ -179,12 +179,12 @@ def main():
         if hasattr(model, 'classifier') and isinstance(model.classifier, nn.Module):
             optimizer_tmp = init_optim(args.optim, model.classifier.parameters(), args.fixbase_lr, args.weight_decay)
         else:
-            #print("Warn: model has no attribute 'classifier' and fixbase_epoch is reset to 0")
+            print("Warn: model has no attribute 'classifier' and fixbase_epoch is reset to 0")
             args.fixbase_epoch = 0
 
     if args.load_weights:
         # load pretrained weights but ignore layers that don't match in size
-        #print("Loading pretrained weights from '{}'".format(args.load_weights))
+        print("Loading pretrained weights from '{}'".format(args.load_weights))
         if torch.cuda.is_available():
             checkpoint = torch.load(args.load_weights)
         else :
@@ -200,14 +200,14 @@ def main():
         model.load_state_dict(checkpoint['state_dict'])
         start_epoch = checkpoint['epoch']
         rank1 = checkpoint['rank1']
-        #print("Loaded checkpoint from '{}'".format(args.resume))
-        #print("- start_epoch: {}\n- rank1: {}".format(start_epoch, rank1))
+        print("Loaded checkpoint from '{}'".format(args.resume))
+        print("- start_epoch: {}\n- rank1: {}".format(start_epoch, rank1))
 
     if use_gpu:
         model = nn.DataParallel(model).cuda()
 
     if args.evaluate:
-        #print("Evaluate only")
+        print("Evaluate only")
         test(model, queryloader, galleryloader, use_gpu)
         return
 
@@ -215,10 +215,10 @@ def main():
     train_time = 0
     best_rank1 = -np.inf
     best_epoch = 0
-    #print("==> Start training")
+    print("==> Start training")
 
     if args.fixbase_epoch > 0:
-        #print("Train classifier for {} epochs while keeping base network frozen".format(args.fixbase_epoch))
+        print("Train classifier for {} epochs while keeping base network frozen".format(args.fixbase_epoch))
 
         for epoch in range(args.fixbase_epoch):
             start_train_time = time.time()
@@ -226,7 +226,7 @@ def main():
             train_time += round(time.time() - start_train_time)
 
         del optimizer_tmp
-        #print("Now open all layers for training")
+        print("Now open all layers for training")
 
     for epoch in range(start_epoch, args.max_epoch):
         start_train_time = time.time()
@@ -236,7 +236,7 @@ def main():
         scheduler.step()
         
         if (epoch+1) > args.start_eval and args.eval_step > 0 and (epoch+1) % args.eval_step == 0 or (epoch+1) == args.max_epoch:
-            #print("==> Test")
+            print("==> Test")
             rank1 = test(model, queryloader, galleryloader, use_gpu)
             is_best = rank1 > best_rank1
             
@@ -268,12 +268,12 @@ def main():
         }, True, osp.join(args.save_dir, 'checkpoint_ep' + str(epoch+1) + '.pth.tar'))
         '''
 
-    #print("==> Best Rank-1 {:.1%}, achieved at epoch {}".format(best_rank1, best_epoch))
+    print("==> Best Rank-1 {:.1%}, achieved at epoch {}".format(best_rank1, best_epoch))
 
     elapsed = round(time.time() - start_time)
     elapsed = str(datetime.timedelta(seconds=elapsed))
     train_time = str(datetime.timedelta(seconds=train_time))
-    #print("Finished. Total elapsed time (h:m:s): {}. Training time (h:m:s): {}.".format(elapsed, train_time))
+    print("Finished. Total elapsed time (h:m:s): {}. Training time (h:m:s): {}.".format(elapsed, train_time))
 
 
 def train(epoch, model, criterion, optimizer, trainloader, use_gpu, freeze_bn=False):
@@ -308,7 +308,7 @@ def train(epoch, model, criterion, optimizer, trainloader, use_gpu, freeze_bn=Fa
         losses.update(loss.item(), pids.size(0))
 
         if (batch_idx+1) % args.print_freq == 0:
-            #print('Epoch: [{0}][{1}/{2}]\t'
+            print('Epoch: [{0}][{1}/{2}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Data {data_time.val:.4f} ({data_time.avg:.4f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
@@ -333,16 +333,16 @@ def test(model, queryloader, galleryloader, use_gpu, ranks=[1, 5, 10, 20]):
             batch_time.update(time.time() - end)
             
             features = features.data.cpu()
-            #print('features', features.size(), features)
+            # print('features', features.size(), features)
             qf.append(features)
-            #print('qf', len(qf))
+            # print('qf', len(qf))
             q_pids.extend(pids)
             q_camids.extend(camids)
         qf = torch.cat(qf, 0)
         q_pids = np.asarray(q_pids)
         q_camids = np.asarray(q_camids)
 
-        #print("Extracted features for query set, obtained {}-by-{} matrix".format(qf.size(0), qf.size(1)))
+        print("Extracted features for query set, obtained {}-by-{} matrix".format(qf.size(0), qf.size(1)))
 
         gf, g_pids, g_camids = [], [], []
         end = time.time()
@@ -361,59 +361,59 @@ def test(model, queryloader, galleryloader, use_gpu, ranks=[1, 5, 10, 20]):
         g_pids = np.asarray(g_pids)
         g_camids = np.asarray(g_camids)
 
-        #print("Extracted features for gallery set, obtained {}-by-{} matrix".format(gf.size(0), gf.size(1)))
+        print("Extracted features for gallery set, obtained {}-by-{} matrix".format(gf.size(0), gf.size(1)))
 
-    #print("==> BatchTime(s)/BatchSize(img): {:.3f}/{}".format(batch_time.avg, args.test_batch))
+    print("==> BatchTime(s)/BatchSize(img): {:.3f}/{}".format(batch_time.avg, args.test_batch))
 
-    #print('qf', qf.size(), qf)
-    #print('gf', gf.size(), gf)
+    # print('qf', qf.size(), qf)
+    # print('gf', gf.size(), gf)
 
     m, n = qf.size(0), gf.size(0)
-    #print('m,n',m,n)
-    #print('qf', qf.size(), qf)
-    qf_pow = torch.pow(qf, 2)
-    #print('qf_pow', qf_pow.size(), qf_pow)
-    qf_sum = qf_pow.sum(dim=1, keepdim=True)
-    #print('qf_sum', qf_sum.size(), qf_sum)
-    qf_exp = qf_sum.expand(m, n)
-    #print('qf_exp', qf_exp.size(), qf_exp)
+    # print('m,n',m,n)
+    # print('qf', qf.size(), qf)
+    # qf_pow = torch.pow(qf, 2)
+    # # print('qf_pow', qf_pow.size(), qf_pow)
+    # qf_sum = qf_pow.sum(dim=1, keepdim=True)
+    # # print('qf_sum', qf_sum.size(), qf_sum)
+    # qf_exp = qf_sum.expand(m, n)
+    # # print('qf_exp', qf_exp.size(), qf_exp)
 
-    #print('gf', gf.size(), gf)
-    gf_pow = torch.pow(gf, 2)
-    #print('gf_pow', gf_pow.size(), gf_pow)
-    gf_sum = gf_pow.sum(dim=1, keepdim=True)
-    #print('gf_sum', gf_sum.size(), gf_sum)
-    gf_exp = gf_sum.expand(n, m)
-    #print('gf_exp', gf_exp.size(), gf_exp)
-    gf_t = gf_exp.t()
-    #print('gf_t', gf_t.size(), gf_t)
-    # distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
-    #           torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
-    distmat = qf_exp + gf_t
-    #print('distmat', distmat.size(), distmat)
+    # # print('gf', gf.size(), gf)
+    # gf_pow = torch.pow(gf, 2)
+    # # print('gf_pow', gf_pow.size(), gf_pow)
+    # gf_sum = gf_pow.sum(dim=1, keepdim=True)
+    # # print('gf_sum', gf_sum.size(), gf_sum)
+    # gf_exp = gf_sum.expand(n, m)
+    # # print('gf_exp', gf_exp.size(), gf_exp)
+    # gf_t = gf_exp.t()
+    # print('gf_t', gf_t.size(), gf_t)
+    distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
+              torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+    # distmat = qf_exp + gf_t
+    # # print('distmat', distmat.size(), distmat)
     
-    #print('mm', distmat.size, qf.size, gf.size, gf.t().size)
-    qf = qf.numpy()
-    gf = gf.numpy()
-    mm = np.dot(qf, gf.T)
-    # mm = torch.mm(qf, gf.t())
-    #print('mm', mm.shape, distmat.shape, qf.shape, gf.T.shape)
-    distmat = distmat.numpy() + mm*(-2)
+    # # print('mm', distmat.size, qf.size, gf.size, gf.t().size)
+    # qf = qf.numpy()
+    # gf = gf.numpy()
+    # mm = np.dot(qf, gf.T)
+    # # mm = torch.mm(qf, gf.t())
+    # print('mm', mm.shape, distmat.shape, qf.shape, gf.T.shape)
+    # distmat = distmat.numpy() + mm*(-2)
     
-    # distmat.addmm_(1, -2, qf, gf.t())
-    #print('distmat', distmat.shape, distmat)
-    # distmat = distmat.numpy()
+    # # distmat.addmm_(1, -2, qf, gf.t())
+    # print('distmat', distmat.shape, distmat)
+    # # distmat = distmat.numpy()
 
-    #print("Computing CMC and mAP")
+    print("Computing CMC and mAP")
     # cmc, mAP = evaluate(distmat, q_pids, g_pids, q_camids, g_camids, use_metric_cuhk03=args.use_metric_cuhk03)
     cmc, mAP = evaluate(distmat, q_pids, g_pids, q_camids, g_camids, dataset_type=args.dataset)
 
-    #print("Results ----------")
-    #print("mAP: {:.1%}".format(mAP))
-    #print("CMC curve")
+    print("Results ----------")
+    print("mAP: {:.1%}".format(mAP))
+    print("CMC curve")
     for r in ranks:
-        #print("Rank-{:<3}: {:.1%}".format(r, cmc[r-1]))
-    #print("------------------")
+        print("Rank-{:<3}: {:.1%}".format(r, cmc[r-1]))
+    print("------------------")
 
     return cmc[0]
 
