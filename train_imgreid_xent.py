6a1fe73910253a5d72cb85988395af0b92e4ea22
luke <zzanzza@finger-plus.com>
2018-10-05 23:53:34 +0900

    bug수정

--- train_imgreid_xent.py
+++ train_imgreid_xent.py
@@ -15,18 +15,18 @@ import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
 
-from torchreid import data_manager
-from torchreid.dataset_loader import ImageDataset
-from torchreid import transforms as T
-from torchreid import models
-from torchreid.losses import CrossEntropyLabelSmooth, DeepSupervision
-from torchreid.utils.iotools import save_checkpoint, check_isfile
-from torchreid.utils.avgmeter import AverageMeter
-from torchreid.utils.logger import Logger
-from torchreid.utils.torchtools import set_bn_to_eval, count_num_param
-from torchreid.utils.reidtools import visualize_ranked_results
-from torchreid.eval_metrics import evaluate
-from torchreid.optimizers import init_optim
+import data_manager
+from dataset_loader import ImageDataset
+import transforms as T
+import models
+from losses import CrossEntropyLabelSmooth, DeepSupervision
+from utils.iotools import save_checkpoint
+from utils.avgmeter import AverageMeter
+from utils.logger import Logger
+from utils.torchtools import set_bn_to_eval, count_num_param
+from eval_metrics import evaluate
+from optimizers import init_optim
+# from torchsummary import summary
 
 
 parser = argparse.ArgumentParser(description='Train image model with cross entropy loss')
@@ -42,14 +42,16 @@ parser.add_argument('--height', type=int, default=256,
 parser.add_argument('--width', type=int, default=128,
                     help="width of an image (default: 128)")
 parser.add_argument('--split-id', type=int, default=0,
-                    help="split index (0-based)")
+                    help="split index")
+parser.add_argument('--use-lmdb', action='store_true',
+                    help="whether to use lmdb dataset")
 # CUHK03-specific setting
 parser.add_argument('--cuhk03-labeled', action='store_true',
-                    help="use labeled images, if false, detected images are used (default: False)")
+                    help="whether to use labeled images, if false, detected images are used (default: False)")
 parser.add_argument('--cuhk03-classic-split', action='store_true',
-                    help="use classic split by Li et al. CVPR'14 (default: False)")
+                    help="whether to use classic split by Li et al. CVPR'14 (default: False)")
 parser.add_argument('--use-metric-cuhk03', action='store_true',
-                    help="use cuhk03-metric (default: False)")
+                    help="whether to use cuhk03-metric (default: False)")
 # Optimization options
 parser.add_argument('--optim', type=str, default='adam',
                     help="optimization algorithm (see optimizers.py)")
@@ -75,8 +77,6 @@ parser.add_argument('--fixbase-lr', default=0.0003, type=float,
                     help="learning rate (when base network is frozen)")
 parser.add_argument('--freeze-bn', action='store_true',
                     help="freeze running statistics in BatchNorm layers during training (default: False)")
-parser.add_argument('--label-smooth', action='store_true',
-                    help="use label smoothing regularizer in cross entropy loss")
 # Architecture
 parser.add_argument('-a', '--arch', type=str, default='resnet50', choices=models.get_names())
 # Miscs
@@ -98,19 +98,13 @@ parser.add_argument('--use-cpu', action='store_true',
                     help="use cpu")
 parser.add_argument('--gpu-devices', default='0', type=str,
                     help='gpu device ids for CUDA_VISIBLE_DEVICES')
-parser.add_argument('--use-avai-gpus', action='store_true',
-                    help="use available gpus instead of specified devices (this is useful when using managed clusters)")
-parser.add_argument('--vis-ranked-res', action='store_true',
-                    help="visualize ranked results, only available in evaluation mode (default: False)")
 
-# global variables
 args = parser.parse_args()
-best_rank1 = -np.inf
 
 
 def main():
     torch.manual_seed(args.seed)
-    if not args.use_avai_gpus: os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_devices
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_devices
     use_gpu = torch.cuda.is_available()
     if args.use_cpu: use_gpu = False
 
@@ -131,10 +125,12 @@ def main():
     dataset = data_manager.init_imgreid_dataset(
         root=args.root, name=args.dataset, split_id=args.split_id,
         cuhk03_labeled=args.cuhk03_labeled, cuhk03_classic_split=args.cuhk03_classic_split,
+        use_lmdb=args.use_lmdb,
     )
 
     transform_train = T.Compose([
         T.Random2DTranslation(args.height, args.width),
+        # T.Resize((args.height, args.width)),
         T.RandomHorizontalFlip(),
         T.ToTensor(),
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -148,20 +144,23 @@ def main():
 
     pin_memory = True if use_gpu else False
 
+    trainset = ImageDataset(dataset.train, transform=transform_train, use_lmdb=args.use_lmdb, lmdb_path=dataset.train_lmdb_path)
     trainloader = DataLoader(
-        ImageDataset(dataset.train, transform=transform_train),
+        trainset,
         batch_size=args.train_batch, shuffle=True, num_workers=args.workers,
         pin_memory=pin_memory, drop_last=True,
     )
 
+    queryset = ImageDataset(dataset.query, transform=transform_test, use_lmdb=args.use_lmdb, lmdb_path=dataset.query_lmdb_path)
     queryloader = DataLoader(
-        ImageDataset(dataset.query, transform=transform_test),
+        queryset,
         batch_size=args.test_batch, shuffle=False, num_workers=args.workers,
         pin_memory=pin_memory, drop_last=False,
     )
 
+    galleryset = ImageDataset(dataset.gallery, transform=transform_test, use_lmdb=args.use_lmdb, lmdb_path=dataset.gallery_lmdb_path)
     galleryloader = DataLoader(
-        ImageDataset(dataset.gallery, transform=transform_test),
+        galleryset,
         batch_size=args.test_batch, shuffle=False, num_workers=args.workers,
         pin_memory=pin_memory, drop_last=False,
     )
@@ -169,13 +168,12 @@ def main():
     print("Initializing model: {}".format(args.arch))
     model = models.init_model(name=args.arch, num_classes=dataset.num_train_pids, loss={'xent'}, use_gpu=use_gpu)
     print("Model size: {:.3f} M".format(count_num_param(model)))
+    # summary(model, (3, 160, 64))
 
-    if args.label_smooth:
-        criterion = CrossEntropyLabelSmooth(num_classes=dataset.num_train_pids, use_gpu=use_gpu)
-    else:
-        criterion = nn.CrossEntropyLoss()
+    criterion = CrossEntropyLabelSmooth(num_classes=dataset.num_train_pids, use_gpu=use_gpu)
     optimizer = init_optim(args.optim, model.parameters(), args.lr, args.weight_decay)
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=args.stepsize, gamma=args.gamma)
+    start_epoch = args.start_epoch
 
     if args.fixbase_epoch > 0:
         if hasattr(model, 'classifier') and isinstance(model.classifier, nn.Module):
@@ -184,41 +182,39 @@ def main():
             print("Warn: model has no attribute 'classifier' and fixbase_epoch is reset to 0")
             args.fixbase_epoch = 0
 
-    if args.load_weights and check_isfile(args.load_weights):
+    if args.load_weights:
         # load pretrained weights but ignore layers that don't match in size
-        checkpoint = torch.load(args.load_weights)
+        print("Loading pretrained weights from '{}'".format(args.load_weights))
+        if torch.cuda.is_available():
+            checkpoint = torch.load(args.load_weights)
+        else :
+            checkpoint = torch.load(args.load_weights, map_location='cpu')
         pretrain_dict = checkpoint['state_dict']
         model_dict = model.state_dict()
         pretrain_dict = {k: v for k, v in pretrain_dict.items() if k in model_dict and model_dict[k].size() == v.size()}
         model_dict.update(pretrain_dict)
         model.load_state_dict(model_dict)
-        print("Loaded pretrained weights from '{}'".format(args.load_weights))
 
-    if args.resume and check_isfile(args.resume):
+    if args.resume:
         checkpoint = torch.load(args.resume)
         model.load_state_dict(checkpoint['state_dict'])
-        args.start_epoch = checkpoint['epoch'] + 1
-        best_rank1 = checkpoint['rank1']
+        start_epoch = checkpoint['epoch']
+        rank1 = checkpoint['rank1']
         print("Loaded checkpoint from '{}'".format(args.resume))
-        print("- start_epoch: {}\n- rank1: {}".format(args.start_epoch, best_rank1))
+        print("- start_epoch: {}\n- rank1: {}".format(start_epoch, rank1))
 
     if use_gpu:
         model = nn.DataParallel(model).cuda()
 
     if args.evaluate:
         print("Evaluate only")
-        distmat = test(model, queryloader, galleryloader, use_gpu, return_distmat=True)
-        if args.vis_ranked_res:
-            visualize_ranked_results(
-                distmat, dataset,
-                save_dir=osp.join(args.save_dir, 'ranked_results'),
-                topk=20,
-            )
+        test(model, queryloader, galleryloader, use_gpu)
         return
 
     start_time = time.time()
     train_time = 0
-    best_epoch = args.start_epoch
+    best_rank1 = -np.inf
+    best_epoch = 0
     print("==> Start training")
 
     if args.fixbase_epoch > 0:
@@ -232,14 +228,14 @@ def main():
         del optimizer_tmp
         print("Now open all layers for training")
 
-    for epoch in range(args.start_epoch, args.max_epoch):
+    for epoch in range(start_epoch, args.max_epoch):
         start_train_time = time.time()
         train(epoch, model, criterion, optimizer, trainloader, use_gpu)
         train_time += round(time.time() - start_train_time)
         
         scheduler.step()
         
-        if (epoch + 1) > args.start_eval and args.eval_step > 0 and (epoch + 1) % args.eval_step == 0 or (epoch + 1) == args.max_epoch:
+        if (epoch+1) > args.start_eval and args.eval_step > 0 and (epoch+1) % args.eval_step == 0 or (epoch+1) == args.max_epoch:
             print("==> Test")
             rank1 = test(model, queryloader, galleryloader, use_gpu)
             is_best = rank1 > best_rank1
@@ -257,7 +253,20 @@ def main():
                 'state_dict': state_dict,
                 'rank1': rank1,
                 'epoch': epoch,
-            }, is_best, osp.join(args.save_dir, 'checkpoint_ep' + str(epoch + 1) + '.pth.tar'))
+            }, is_best, osp.join(args.save_dir, 'checkpoint_ep' + str(epoch+1) + '.pth.tar'))
+        
+        
+        '''
+        if use_gpu:
+            state_dict = model.module.state_dict()
+        else:
+            state_dict = model.state_dict()
+            
+        save_checkpoint({
+            'state_dict': state_dict,
+            'epoch': epoch,
+        }, True, osp.join(args.save_dir, 'checkpoint_ep' + str(epoch+1) + '.pth.tar'))
+        '''
 
     print("==> Best Rank-1 {:.1%}, achieved at epoch {}".format(best_rank1, best_epoch))
 
@@ -285,6 +294,7 @@ def train(epoch, model, criterion, optimizer, trainloader, use_gpu, freeze_bn=Fa
             imgs, pids = imgs.cuda(), pids.cuda()
         
         outputs = model(imgs)
+        
         if isinstance(outputs, tuple):
             loss = DeepSupervision(criterion, outputs, pids)
         else:
@@ -297,18 +307,18 @@ def train(epoch, model, criterion, optimizer, trainloader, use_gpu, freeze_bn=Fa
 
         losses.update(loss.item(), pids.size(0))
 
-        if (batch_idx + 1) % args.print_freq == 0:
+        if (batch_idx+1) % args.print_freq == 0:
             print('Epoch: [{0}][{1}/{2}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Data {data_time.val:.4f} ({data_time.avg:.4f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
-                   epoch + 1, batch_idx + 1, len(trainloader), batch_time=batch_time,
+                   epoch+1, batch_idx+1, len(trainloader), batch_time=batch_time,
                    data_time=data_time, loss=losses))
         
         end = time.time()
 
 
-def test(model, queryloader, galleryloader, use_gpu, ranks=[1, 5, 10, 20], return_distmat=False):
+def test(model, queryloader, galleryloader, use_gpu, ranks=[1, 5, 10, 20]):
     batch_time = AverageMeter()
     
     model.eval()
@@ -323,7 +333,9 @@ def test(model, queryloader, galleryloader, use_gpu, ranks=[1, 5, 10, 20], retur
             batch_time.update(time.time() - end)
             
             features = features.data.cpu()
+            # print('features', features.size(), features)
             qf.append(features)
+            # print('qf', len(qf))
             q_pids.extend(pids)
             q_camids.extend(camids)
         qf = torch.cat(qf, 0)
@@ -353,14 +365,48 @@ def test(model, queryloader, galleryloader, use_gpu, ranks=[1, 5, 10, 20], retur
 
     print("==> BatchTime(s)/BatchSize(img): {:.3f}/{}".format(batch_time.avg, args.test_batch))
 
+    # print('qf', qf.size(), qf)
+    # print('gf', gf.size(), gf)
+
     m, n = qf.size(0), gf.size(0)
+    # print('m,n',m,n)
+    # print('qf', qf.size(), qf)
+    # qf_pow = torch.pow(qf, 2)
+    # # print('qf_pow', qf_pow.size(), qf_pow)
+    # qf_sum = qf_pow.sum(dim=1, keepdim=True)
+    # # print('qf_sum', qf_sum.size(), qf_sum)
+    # qf_exp = qf_sum.expand(m, n)
+    # # print('qf_exp', qf_exp.size(), qf_exp)
+
+    # # print('gf', gf.size(), gf)
+    # gf_pow = torch.pow(gf, 2)
+    # # print('gf_pow', gf_pow.size(), gf_pow)
+    # gf_sum = gf_pow.sum(dim=1, keepdim=True)
+    # # print('gf_sum', gf_sum.size(), gf_sum)
+    # gf_exp = gf_sum.expand(n, m)
+    # # print('gf_exp', gf_exp.size(), gf_exp)
+    # gf_t = gf_exp.t()
+    # print('gf_t', gf_t.size(), gf_t)
     distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
               torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+    # distmat = qf_exp + gf_t
+    # # print('distmat', distmat.size(), distmat)
+    
+    # # print('mm', distmat.size, qf.size, gf.size, gf.t().size)
+    # qf = qf.numpy()
+    # gf = gf.numpy()
+    # mm = np.dot(qf, gf.T)
+    # # mm = torch.mm(qf, gf.t())
+    # print('mm', mm.shape, distmat.shape, qf.shape, gf.T.shape)
+    # distmat = distmat.numpy() + mm*(-2)
+    
     distmat.addmm_(1, -2, qf, gf.t())
+    # print('distmat', distmat.shape, distmat)
     distmat = distmat.numpy()
 
     print("Computing CMC and mAP")
-    cmc, mAP = evaluate(distmat, q_pids, g_pids, q_camids, g_camids, use_metric_cuhk03=args.use_metric_cuhk03)
+    # cmc, mAP = evaluate(distmat, q_pids, g_pids, q_camids, g_camids, use_metric_cuhk03=args.use_metric_cuhk03)
+    cmc, mAP = evaluate(distmat, q_pids, g_pids, q_camids, g_camids, dataset_type=args.dataset)
 
     print("Results ----------")
     print("mAP: {:.1%}".format(mAP))
@@ -369,8 +415,6 @@ def test(model, queryloader, galleryloader, use_gpu, ranks=[1, 5, 10, 20], retur
         print("Rank-{:<3}: {:.1%}".format(r, cmc[r-1]))
     print("------------------")
 
-    if return_distmat:
-        return distmat
     return cmc[0]
 
 
